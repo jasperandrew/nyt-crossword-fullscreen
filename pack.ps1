78015135498823af3f5cc79e1999a8122366e502
Jasper Andrew <9101110+jasperandrew@users.noreply.github.com>
2024-02-18 19:15:38 -0500

    delete helper script, since apparently mozilla's scanner can't cope

--- pack.ps1
+++
@@ -1,6 +0,0 @@
-param([int]$v=2)
-
-Remove-Item ".\package.zip" 2>$null
-Copy-Item "manifest.v$v.json" -Destination ".\package\manifest.json"
-Compress-Archive ".\package\*" -DestinationPath ".\package.zip"
-Remove-Item ".\package\manifest.json"
